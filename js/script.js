539002c14e1fc9e690fe2a818e893420881e557c
akhil <aj17kr@gmail.com>
2019-06-13 00:36:37 +0530

    Updated site with new branch details

--- js/script.js
+++ js/script.js
@@ -1,26 +1,66 @@
 // ======================GOOGLE MAPS INTEGRATION======================= //
 
-		var myCenter = new google.maps.LatLng(28.459545135498047,77.04367065429688);
+		// var myCenter = new google.maps.LatLng(28.459545135498047,77.04367065429688);
 
-		function initialize() {
-		var mapProp = {
-		center:myCenter,
-		zoom:15,
-		scrollwheel:false,
-		draggable:true,
-		mapTypeId:google.maps.MapTypeId.ROADMAP
-		};
+		// function initialize() {
+		// var mapProp = {
+		// center:myCenter,
+		// zoom:15,
+		// scrollwheel:false,
+		// draggable:true,
+		// mapTypeId:google.maps.MapTypeId.ROADMAP
+		// };
 
-		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-		var marker = new google.maps.Marker({
-		position:myCenter,
-		});
+		// var marker = new google.maps.Marker({
+		// position:myCenter,
+		// });
 
-		marker.setMap(map);
-		}
+		// marker.setMap(map);
+		// }
 
-		google.maps.event.addDomListener(window, 'load', initialize);
+		// google.maps.event.addDomListener(window, 'load', initialize);
+// ======================/GOOGLE MAPS INTEGRATION======================= //
+
+// ======================GOOGLE MAPS INTEGRATION======================= //
+
+    var myCenter1 = new google.maps.LatLng(28.459545135498047,77.04367065429688);
+    var myCenter2 = new google.maps.LatLng(28.460180,77.000763);
+
+    function initialize() {
+    var mapProp1 = {
+    center:myCenter1,
+    zoom:15,
+    scrollwheel:false,
+    draggable:true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+
+    var mapProp2 = {
+    center:myCenter2,
+    zoom:15,
+    scrollwheel:false,
+    draggable:true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+
+    var map1 = new google.maps.Map(document.getElementById("googleMap1"),mapProp1);
+    var map2 = new google.maps.Map(document.getElementById("googleMap2"),mapProp2);
+
+    var marker1 = new google.maps.Marker({
+    position:myCenter1,
+    });
+
+    var marker2 = new google.maps.Marker({
+    position:myCenter2,
+    });
+
+    marker1.setMap(map1);
+    marker2.setMap(map2);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 // ======================/GOOGLE MAPS INTEGRATION======================= //
 
 
